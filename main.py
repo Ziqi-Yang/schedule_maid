0735958a4811b01b527a771eeb9fb788ef7e44df
Ziqi-Yang <mr.ziqiyang@gmail.com>
2021-11-15 17:37:23 +0800

    normal

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import yaml
 from yaml.loader import Loader
 
 # read config from config.yaml
-with open("config.yaml") as f:
+with open("_config.yaml") as f:
     a = yaml.load(f.read(),Loader=Loader)
-    
