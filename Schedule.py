4740e860b4c7f2151ede6c29ef7a6cfa9e74348d
Ziqi-Yang <mr.ziqiyang@gmail.com>
2021-11-16 19:53:57 +0800

    normal

--- Schedule.py
+++ Schedule.py
@@ -1,12 +1,13 @@
 from pandas import read_excel
 from datetime import datetime
 import scheduleTime
+import re
 
 
 
 class Schedule:
     def __init__(self,excel_path):
-        self.schedule = read_excel(excel_path,engine="openpyxl",index_col=0)
+        self.schedule = read_excel(excel_path,engine="openpyxl",index_col=r"时间\星期")
 
 
     def check_sch(self):
@@ -18,9 +19,47 @@ class Schedule:
         """
         # FIXME need to support label
         self.scheduled_times = scheduleTime.ScheduleTime(self.schedule.index[1:])
+    
 
     def getTodaySch(self,weekday):
         self.todaySch = self.schedule.iloc[:,weekday]
+
+    def parseCell(self,text):
+        lines =  text.split("\n")
+        mode = lines[0] if lines[0] in ["[multi]","[section]"] else None
+        if mode == None: # normal Cell
+            message = "\n".join(lines)
+            for line in lines:
+                if line.startswith("[") and line.endswith("]"):
+                    continue
+                else:
+                    self.parseLine()
+        else:
+            if mode == "[multi]":
+                pass
+            elif mode == "[section]":
+                pass
+    
+    def parseLine(self,line):
+        line = line.strip()
+        tmp = re.findall(r"(\d{2}[.:]\d{2})\s[-](\d{2}[.:]\d{2})",line)
+        if len(tmp) == 1:
+            tmp = [i for x in tmp for i in x]
+            if len(tmp) != 2:
+                raise Exception("the format of {} is not vailed".format(line))
+            else:
+                if tmp[0] != tmp[1]:
+                    timeStr_lastPosPlus1 = re.search(tmp[1],line).span()[-1]
+                    
+                    print(tmp[0],tmp[1],line,timeStr_lastPosPlus1)
+                else:
+                    raise Exception("{} contains same timeString".format(line))
+        else:
+            raise Exception("the format of {} is not vailed".format(line))
+    
+    def querySch(self,time:datetime):
+        pass
+        
         
     
 if __name__ == "__main__":
@@ -28,4 +67,4 @@ if __name__ == "__main__":
     weekday = datetime.today().weekday()
     sch.getTodaySch(weekday)
     sch.check_sch()
-    print(sch.todaySch)
+    sch.parseLine(" 16.00 -17:00鸟哥的Linux私房菜")
